46cb79a54b48e5dd3b123e6f77cf0ae81f7a7ca3
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-05-05 00:32:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1" # Replace with your desired region
+}
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-repo"
+  image_tag_mutability = "MUTABLE"  # or "IMMUTABLE" based on your requirement
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
