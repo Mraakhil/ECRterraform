ab44781b311d459f3515a0e901ae66a9f3e1a225
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-05-05 01:17:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
-provider "aws" {
-  region = "us-east-1" # Replace with your desired region
-}
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = "my-ecr-repo"
-  image_tag_mutability = "MUTABLE"  # or "IMMUTABLE" based on your requirement
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+resource "aws_ecr_lifecycle_policy" "my_policy" {
+  repository = aws_ecr_repository.my_ecr_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rule_priority = 1
+        description   = "Keep only 10 images"
+        selection     = {
+          count_type        = "imageCountMoreThan"
+          count_number      = 10
+          tag_status        = "tagged"
+          tag_prefix_list   = ["prod"]
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
