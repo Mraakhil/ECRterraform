963badb68fd5928b68a07e852af597803463bf16
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-05-05 14:15:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAVVZOOI3AISHJUVQO"
-  secret_key = "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"
+  
 }
 resource "aws_ecr_repository" "my_ecr_repo" {
   name                 = "my-ecr-repo"
